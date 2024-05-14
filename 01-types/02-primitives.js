a71db564bbfc50256525462736e99fddea8f4460
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 08:46:49 -0500

    Add more explain.

---
+++ 01-types/02-primitives.js
@@ -0,0 +1,6 @@
+let number = 1;
+let text = "Hola Mundo";
+let verdadero = true;
+let falso = false;
+let notDefined; // any means undefined
+let nulo = null;
---
+++ 01-types/03-constants.js
@@ -0,0 +1,5 @@
+let name = "Hola Mundo"
+
+console.log(name)
+
+//another way of variable, its const -> in this form will say an error if we are gettin a new data in an old variable already used.
