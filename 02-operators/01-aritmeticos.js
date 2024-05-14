813fd0808d603def9d973701272c8b892181c63e
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 13:38:47 -0500

    Desarrollo de explicación sobre aritmeticos, en el cual explica todo.

---
+++ 02-operators/01-aritmeticos.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b); // suma
+console.log(a - b); // resta
+console.log(a * b); // multiplicación
+console.log(a / b); // división
+console.log(a % b); // módulo
+console.log(a ** b); // potencia
+
+
+// console.log (a++) --> Aqui empieza a sumar 1. ¿Por que? Pues, resulta que "++"
+// es un operador de incremento, que se usa para incrementar el valor de una variable en 1.
+// Por lo tanto, cuando se ejecuta el código, el valor de la variable "a" se incrementa en 1, y el resultado de la operación es 6.
