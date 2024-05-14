e38101937bd60a12b83ada93888d9553442075a6
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 08:53:45 -0500

    Here you can know what kind of text is each one.

---
+++ 01-types/04-typed-dinamic.js
@@ -0,0 +1,11 @@
+let number = 42;
+let name = "Hola Mundo";
+let verdadero = true;
+let undef;
+let nula = null;
+
+console.log(typeof number)
+console.log(typeof name)
+console.log(typeof verdadero)
+console.log(typeof undef)
+console.log(typeof nula)
