16bb72bd8b7faade7706275d0a9968626bc39d3d
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 13:10:06 -0500

    End of arguments, more explaining about how to do a more open plus or function.

--- 01-types/09-arguments.js
+++ 01-types/09-arguments.js
@@ -1,8 +1,11 @@
-function plus(a) {
-    return a + 2;
+function plus(a, b) {
+    return a + b;
 }
 // Here, we are going to let the function grab "a" and let it empty for use it later.
 
 // And here, we are giving a value to "a".
-let results = plus(5);
+let results = plus(5, 6);
 console.log(results);
+
+console.log(typeof plus);
+//Function
