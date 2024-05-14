992d8835e4d3010c19147335701ddacf99b2da35
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 08:10:01 -0500

    Update with explain

---
+++ 01-types/01-variables.js
@@ -0,0 +1,5 @@
+let namevariable = "Hola Mundo";
+
+// EXPLAIN....         In this moment, to put a name to a variable
+// we have to know that it always gonna start with a letter or with a ground bar ( _ )
+console.log(namevariable);
