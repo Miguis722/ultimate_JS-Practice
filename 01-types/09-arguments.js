076481619a87fe4fb082df32198f9353df0f8ca4
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 13:05:47 -0500

    Explain of arguments

---
+++ 01-types/09-arguments.js
@@ -0,0 +1,8 @@
+function plus(a) {
+    return a + 2;
+}
+// Here, we are going to let the function grab "a" and let it empty for use it later.
+
+// And here, we are giving a value to "a".
+let results = plus(5);
+console.log(results);
