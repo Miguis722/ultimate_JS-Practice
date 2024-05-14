659d5aca85d2f698e758357c24218287b47a0b94
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 09:49:29 -0500

    Change in the index, and add new section. Arrays.

---
+++ 01-types/07-arrays.js
@@ -0,0 +1,15 @@
+let singers = ['Fleetwood', 'Childish'];
+
+console.log(singers);
+console.log(singers[0]);
+console.log(singers[1]);
+
+// If we are going to add a new data inside of the group
+singers[2] = 'Weeknd';
+console.log(singers)
+
+//  Array inside of JS acts like a objects
+console.log(typeof singers);
+
+// How many elements contents an array
+console.log(singers.length)
