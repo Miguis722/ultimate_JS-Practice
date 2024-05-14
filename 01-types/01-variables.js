d5591032a7792696a7b3bd92a9b7b0451f023aa1
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-05-14 08:27:38 -0500

    Explain, and i put an example.

--- 01-types/01-variables.js
+++ 01-types/01-variables.js
@@ -2,4 +2,21 @@ let namevariable = "Hola Mundo";
 
 // EXPLAIN....         In this moment, to put a name to a variable
 // we have to know that it always gonna start with a letter or with a ground bar ( _ )
+
+UpperCamelCase, camelCase, snake_case
+// The first
+
+// UpperCamelcase
+let CompleteName;
+// camelCase
+let completeName; // this is the most ussed in the WORLD of the programming
+// snake_case
+let complete_name;
+
+
+// In JavaScript, we can call a variable without information, and later call the variable and put the content.
+// Example:
+let animal;
+animal = 'Dog'
+
 console.log(namevariable);
